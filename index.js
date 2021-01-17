c1287ce5a1b3c0a035c6a29c0e5b0fdeab2a7f93
mehul9595 <mehul9595@gmail.com>
2021-01-17 09:09:39 +0800

    socket server connection

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const express = require('express');
+const express = require("express");
+const http = require("http").Server(express);
+const io = require("socketio")(http);
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
